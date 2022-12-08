224f63d027077865a957c306c6baeb2966e25357
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2022-12-08 00:56:20 -0300

    refactor: form and toast added

--- frontend/src/components/pages/NewUser.tsx
+++ frontend/src/components/pages/NewUser.tsx
@@ -1,74 +1,102 @@
 /* eslint-disable prettier/prettier */
 import { Button, TextField } from '@mui/material'
-import React from 'react'
 import { SubmitHandler, useForm } from 'react-hook-form'
 import { User } from 'utils/types/user'
 import SendIcon from '@mui/icons-material/Send'
 import { api } from 'services/api'
+import { toast } from 'react-toastify'
+import { useNavigate } from 'react-router-dom'
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
+
+const userSchema = yup.object({
+  name: yup.string().required('Preencha o nome do usuário'),
+  address: yup.string().required('Preencha o endereço do usuário'),
+  phone: yup.string().required('Preencha o telefone do usuário'),
+  age: yup.number().max(200).required('Preencha a idade do usuário'),
+  nickName: yup.string().required('Preencha o apelido do usuário')
+})
 
 export default function NewUser() {
+  const navigate = useNavigate()
+
   const {
     register,
     handleSubmit,
     formState: { errors }
-  } = useForm<User>()
-  const onSubmit: SubmitHandler<User> = async (data: User) => {
-    await api
-      .post('/users/new', data)
-    console.log('Usuário criado!')
-  }
-  console.log(errors)
+  } = useForm<User>({ resolver: yupResolver(userSchema) })
 
+  const onSubmit: SubmitHandler<User> =  (data: User) => {
+    api.post('/users/new/', data).then(({ status, data }) => {
+        if (status === 201) {
+          toast.success(data.message)
+          navigate('/')
+        }
+      })
+      .catch(({ response }) => {
+        toast.error(response.data.error)
+      })
+  }
+  
   return (
     <div className="flex flex-col items-center">
       <div>
-        <h1 className="text-xl">Novo Usuário</h1>
+        <h1 className="text-xl font-bold">Novo Usuário</h1>
       </div>
       <form
-        className="flex w-80 flex-col items-center"
+        className="flex w-96 flex-col items-center"
         onSubmit={handleSubmit(onSubmit)}
       >
         <TextField
-          required
           id="name"
-          label="Nome"
+          label="Nome*"
           variant="standard"
-          {...register('name', { required: true })}
+          {...register('name')}
           margin="normal"
+          fullWidth
         />
+        {errors.name && <p className="text-red-500">{errors.name.message}</p>}
         <TextField
-          required
           id="address"
-          label="Endereço"
+          label="Endereço*"
           variant="standard"
-          {...register('address', { required: true })}
+          {...register('address')}
           margin="normal"
+          fullWidth
         />
+        {errors.address && (
+          <p className="text-red-500">{errors.address.message}</p>
+        )}
         <TextField
-          required
           id="phone"
-          label="Telefone"
+          label="Telefone*"
           variant="standard"
-          {...register('phone', { required: true, maxLength: 15 })}
+          {...register('phone')}
           margin="normal"
+          fullWidth
         />
+        {errors.phone && <p className="text-red-500">{errors.phone.message}</p>}
         <TextField
-          required
           type="number"
           id="age"
-          label="Idade"
+          label="Idade*"
           variant="standard"
-          {...register('age', { required: true, maxLength: 15 })}
+          {...register('age')}
           margin="normal"
+          fullWidth
         />
+        {errors.age && <p className="text-red-500">{errors.age.message}</p>}
         <TextField
-          required
           id="nickName"
-          label="Apelido"
+          label="Apelido*"
           variant="standard"
-          {...register('nickName', { required: true })}
+          {...register('nickName')}
           margin="normal"
+          fullWidth
         />
+         {errors.nickName && (
+          <p className="text-red-500">{errors.nickName.message}</p>
+        )}
         <Button type="submit" variant="contained" endIcon={<SendIcon />}>
           Criar Usuário
         </Button>
